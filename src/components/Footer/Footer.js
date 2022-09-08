8af2fdd2040b40f5aeccdb657611a8783a411ea3
bevuxyna <wannhorn@gmail.com>
2022-09-08 21:23:04 +0300

    Сверстать Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <section className="footer">
+            <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+            <div className="footer__info">
+                <p className="footer__copyright">&copy;&nbsp;{new Date().getFullYear()}</p>
+                <div className="footer__links">
+                    <a href="https://practicum.yandex.ru" target="_blank" className="footer__link">Яндекс.Практикум</a>
+                    <a href="https://github.com/bevuxyna" target="_blank" className="footer__link">Github</a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Footer;
