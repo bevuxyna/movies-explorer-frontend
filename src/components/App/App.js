a35761b04032c9e7877ee5f92aa5e3e0ff49bb90
bevuxyna <wannhorn@gmail.com>
2022-09-11 15:01:23 +0300

    commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
@@ -8,41 +8,52 @@ import Login from "../Login/Login";
 import Movies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies";
 import Profile from "../Profile/Profile";
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 function App() {
 
+    const headerEndpoints = ["/movies", "/saved-movies", "/profile", "/"];
+    const footerEndpoints = ["/movies", "/saved-movies", "/"];
+
     return (
             <div className="page">
+                <Route exact path={headerEndpoints}>
                     <Header />
+                </Route>
 
-                    <Routes>
-                        <Route exact path="/">
-                            <Main />
-                        </Route>
+                <Switch>
+                    <Route exact path="/">
+                        <Main />
+                    </Route>
 
-                        <Route exact path="/sign-up">
-                            <Register />
-                        </Route>
+                    <Route exact path="/sign-up">
+                        <Register />
+                    </Route>
 
-                        <Route exact path="/sign-in">
-                            <Login />
-                        </Route>
+                    <Route exact path="/sign-in">
+                        <Login />
+                    </Route>
 
-                        <Route exact path="/profile">
-                            <Profile />
-                        </Route>
+                    <Route exact path="/profile">
+                        <Profile />
+                    </Route>
 
-                        <Route exact path="/movies">
-                            <Movies />
-                        </Route>
+                    <Route exact path="/movies">
+                        <Movies />
+                    </Route>
 
-                        <Route exact path="/saved-movies">
-                            <SavedMovies />
-                        </Route>
+                    <Route exact path="/saved-movies">
+                        <SavedMovies />
+                    </Route>
 
-                    </Routes>
+                    <Route path="*">
+                        <PageNotFound />
+                    </Route>
+                </Switch>
 
+                <Route exact path={footerEndpoints}>
                     <Footer/>
+                </Route>
 
             </div>
     );
