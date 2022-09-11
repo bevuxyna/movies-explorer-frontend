b180e6ee0664797dc5409c72db36add203b019b4
bevuxyna <wannhorn@gmail.com>
2022-09-11 15:00:21 +0300

    Сверстать FilterCheckbox

---
+++ src/components/Movies/FilterCheckbox/FilterCheckbox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FilterCheckbox () {
+    return (
+        <div className="filter-checkbox">
+            <button className="filter-checkbox__button" type=""/>
+            <p className="filter-checkbox__title">Короткометражки</p>
+
+        </div>
+    )
+}
+
+export default FilterCheckbox;
