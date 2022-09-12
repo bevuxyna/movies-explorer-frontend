d6ae7c04a6872a05d31d4d46b46a61b12c401e61
bevuxyna <wannhorn@gmail.com>
2022-09-12 20:23:32 +0300

    add Preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
 import SearchForm from "./SearchForm/SearchForm";
+import Preloader from "../Preloader/Preloader";
 
 function Movies() {
     return (
         <section className="movies">
             <SearchForm />
             <MoviesCardList/>
+            {/*<Preloader />*/}
         </section>
     )
 }
