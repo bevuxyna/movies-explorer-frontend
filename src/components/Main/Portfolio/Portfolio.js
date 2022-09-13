037b4acb24e8b2557da1fcc3c5b30cc54c1dd590
bevuxyna <wannhorn@gmail.com>
2022-09-13 20:59:08 +0300

    fix portfolio links

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -6,19 +6,36 @@ function Portfolio() {
             <h2 className="portfolio__title">Портфолио</h2>
             <ul className="portfolio__list">
                 <li className="portfolio__item">
-                    <a href="https://github.com/bevuxyna/how-to-learn" className="portfolio__link" target="_blank">Статичный сайт</a>
-                    <a href="https://github.com/bevuxyna/how-to-learn" className="portfolio__link" target="_blank">&#8599;</a>
+                    <a
+                        href="https://github.com/bevuxyna/how-to-learn"
+                        className="portfolio__link"
+                        target="_blank"
+                    >
+                        Статичный сайт
+                        <p className="portfolio__link-arrow">&#8599;</p>
+                    </a>
                 </li>
 
                 <li className="portfolio__item">
-                    <a href="https://github.com/bevuxyna/russian-travel" className="portfolio__link" target="_blank">Адаптивный сайт</a>
-                    <a href="https://github.com/bevuxyna/russian-travel" className="portfolio__link" target="_blank">&#8599;</a>
+                    <a
+                        href="https://github.com/bevuxyna/russian-travel"
+                        className="portfolio__link"
+                        target="_blank"
+                    >
+                        Адаптивный сайт
+                        <p className="portfolio__link-arrow">&#8599;</p>
+                    </a>
                 </li>
 
                 <li className="portfolio__item">
-                    <a href="https://bevuxyna.students.nomoredomains.sbs" className="portfolio__link" target="_blank">Одностраничное приложение</a>
-                    <a href="https://bevuxyna.students.nomoredomains.sbs" className="portfolio__link" target="_blank">&#8599;</a>
-
+                    <a
+                        href="https://bevuxyna.students.nomoredomains.sbs"
+                        className="portfolio__link"
+                        target="_blank"
+                    >
+                        Одностраничное приложение
+                        <p className="portfolio__link-arrow">&#8599;</p>
+                    </a>
                 </li>
 
             </ul>
