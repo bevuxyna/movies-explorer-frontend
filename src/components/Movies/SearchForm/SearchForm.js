55ef5d0a8a1a2d1a0b2a696e1b03806ac4bcceed
bevuxyna <wannhorn@gmail.com>
2022-09-19 18:00:00 +0300

    fix SearchForm

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -5,11 +5,11 @@ import searchIcon from '../../../images/search_icon.svg';
 function SearchForm () {
     return (
         <div className="search-form">
-            <div className="search-form__container">
+            <form className="search-form__container">
                 <img className="search-form__container_icon" src={searchIcon} alt="Лупа"></img>
                 <input className="search-form__container_input" placeholder="Фильм" required></input>
                 <button className="search-form__container_button" type="submit" />
-            </div>
+            </form>
 
             <FilterCheckbox/>
         </div>
