f9450a2033f66942ba942421b55d8ba75258d8b2
bevuxyna <wannhorn@gmail.com>
2022-09-21 21:17:48 +0300

    add InfoTooltip popup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Route, Switch, useHistory} from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
@@ -9,18 +9,121 @@ import Movies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies";
 import Profile from "../Profile/Profile";
 import PageNotFound from "../PageNotFound/PageNotFound";
+import InfoTooltip from "../InfoTooltip/InfoTooltip";
 
 import moviesApi from "../../utils/MoviesApi";
 import mainApi from "../../utils/MainApi";
 import * as auth from '../../utils/auth.js';
 import {CurrentUserContext} from "../../contexts/CurrentUserContext";
+import imageError from '../../images/image_error.svg';
+import imageSuccess from '../../images/image_success.svg';
 
 function App() {
-
     const headerEndpoints = ["/movies", "/saved-movies", "/profile", "/"];
     const footerEndpoints = ["/movies", "/saved-movies", "/"];
 
+    const [currentUser, setCurrentUser] = useState({});
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [infoTooltipOpen, setInfoTooltipOpen] = useState(false);
+    const [infoTooltipImage, setInfoTooltipImage] = useState(imageSuccess);
+    const [message, setMessage] = useState('');
+
+    const history = useHistory();
+
+    //проверка наличия у пользователя токена
+    function tokenCheck() {
+        const token = localStorage.getItem("jwt");
+        if (token) {
+            mainApi.getUserInfo()
+                .then((res) => {
+                    if (res) {
+                        setCurrentUser({
+                            name: res.name,
+                            email: res.email,
+                            _id: res._id
+                        });
+                        setLoggedIn(true);
+                    }
+                })
+                .catch((err) => {
+                    // if (err.status === 401) {
+                    //     handleSignout();
+                    // } else {
+                    //     handleSignout();
+                    //     setIsTooltipPopupOpen(true);
+                    //     setSuccess(false);
+                    //     setPopupText(`Ошибка ${err.statusText}`);
+                    // }
+                });
+        }
+    }
+
+    useEffect(() => {
+        tokenCheck();
+    }, []);
+
+    function handleRegister(registerData) {
+        auth.register(registerData)
+            .then(() => {
+                //Попап успешной регистрации
+                setInfoTooltipImage(imageSuccess);
+                setMessage('Вы успешно зарегистрировались!');
+                setInfoTooltipOpen(true);
+
+                //Переадресация пользователя на страницу входа
+                history.push('/movies');
+            })
+            .catch((err) => {
+                //Попап ошибки регистрации
+                setInfoTooltipImage(imageError);
+                setMessage('Что-то пошло не так! Попробуйте ещё раз.');
+                setInfoTooltipOpen(true);
+
+                console.log(`Ошибка ${err}`);
+            });
+    }
+
+    function handleLogin(loginData) {
+        auth.authorize(loginData)
+            .then((res) => {
+                if (res.token) {
+                    setLoggedIn(true);
+                    localStorage.setItem('jwt', res.token);
+                    tokenCheck();
+
+                    //Переадресация пользователя на основную страницу со всей функциональностью приложения
+                    history.push('/movies');
+                }
+            })
+            .catch((err) => {
+                //Попап ошибки входа
+                setInfoTooltipImage(imageError);
+                setMessage('Что-то пошло не так! Попробуйте ещё раз.');
+                setInfoTooltipOpen(true);
+
+                console.log(`Ошибка ${err}`);
+            })
+    }
+
+    function handleUpdateUser(data) {
+        mainApi.updateUserInfo(data)
+            .then((res) => {
+                //обновляем стейт currentUser из полученных данных
+                setCurrentUser(res);
+
+                // closeAllPopups();
+            })
+            .catch((err) => {
+                console.log(`Ошибка ${err}`);
+            })
+    }
+
+    const closeAllPopups = () => {
+        setInfoTooltipOpen(false);
+    };
+
     return (
+        <CurrentUserContext.Provider value={currentUser}>
             <div className="page">
                 <Route exact path={headerEndpoints}>
                     <Header />
@@ -32,11 +135,11 @@ function App() {
                     </Route>
 
                     <Route exact path="/sign-up">
-                        <Register />
+                        <Register onRegister={handleRegister} />
                     </Route>
 
                     <Route exact path="/sign-in">
-                        <Login />
+                        <Login onLogin={handleLogin} />
                     </Route>
 
                     <Route exact path="/profile">
@@ -60,7 +163,15 @@ function App() {
                     <Footer/>
                 </Route>
 
+                <InfoTooltip
+                    isOpen={infoTooltipOpen}
+                    onClose={closeAllPopups}
+                    image={infoTooltipImage}
+                    message={message}
+                />
+
             </div>
+        </CurrentUserContext.Provider>
     );
 }
 
