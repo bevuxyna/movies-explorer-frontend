0998134144822dd6cb4f2766f26debb99a012df5
bevuxyna <wannhorn@gmail.com>
2022-09-21 21:15:59 +0300

    add Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from "react-router-dom";
 import logo from '../../images/logo.svg';
 
-function Login() {
+function Login({onLogin}) {
+    const [userData, setUserData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (evt) => {
+        const {name, value} = evt.target;
+        setUserData({
+            ...userData, [name]: value
+        });
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        onLogin(userData);
+        setUserData({email: '', password: ''});
+    }
+
     return (
         <section className="login">
             <Link to='/'><img src={logo} alt="Логотип" className="logo" /></Link>
             <h2 className="login__title">Рады видеть!</h2>
-            <form className="login__form">
+            <form className="login__form" onSubmit={handleSubmit}>
                 <label className="login__form-label">E-mail</label>
                 <input
                     className="login__form-input"
                     name="email"
                     type="email"
                     id="email"
+                    value={userData.email}
+                    onChange={handleChange}
                     required
                 />
 
@@ -23,6 +43,8 @@ function Login() {
                     name="password"
                     type="password"
                     id="password"
+                    value={userData.password}
+                    onChange={handleChange}
                     required
                 />
 
