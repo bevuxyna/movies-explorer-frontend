16000d695b282028f1ce8310cbe60c6202db62e6
bevuxyna <wannhorn@gmail.com>
2022-10-02 21:28:36 +0300

    fix SearchForm: get data from local storage

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,26 +1,46 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {useLocation} from "react-router-dom";
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import searchIcon from '../../../images/search_icon.svg';
 
 function SearchForm ({onSearch, onSubmitCheckbox}) {
     const [inputValue, setInputValue] = useState("");
+    const [isChecked, setIsChecked] = useState(false);
+
+    const location = useLocation();
+
+    // Берем из хранилища название фильма и состояние чекбокса
+    useEffect(() => {
+        if (location.pathname === "/movies") {
+            setInputValue(localStorage.getItem("movieName"));
+            setIsChecked(JSON.parse(localStorage.getItem("checkboxStatus")));
+        } else if (location.pathname === "/saved-movies") {
+            const checkboxStatus = JSON.parse(localStorage.getItem("checkboxStatusSavedMovies"));
+            setIsChecked(checkboxStatus);
+            onSubmitCheckbox(checkboxStatus);
+        }
+    }, [location]);
 
     function handleInputChange(evt) {
         setInputValue(evt.target.value);
     }
 
-    function handleSubmit(evt) {
+    function handleSubmitSearch(evt) {
         evt.preventDefault();
-        onSearch(inputValue);
+        onSearch(inputValue, isChecked);
     }
 
     return (
         <div className="search-form">
             <form
                 className="search-form__container"
-                onSubmit={handleSubmit}
+                onSubmit={handleSubmitSearch}
             >
-                <img className="search-form__container_icon" src={searchIcon} alt="Лупа"></img>
+                <img
+                    className="search-form__container_icon"
+                    src={searchIcon}
+                    alt="Лупа"
+                ></img>
                 <input
                     className="search-form__container_input"
                     placeholder="Фильм"
