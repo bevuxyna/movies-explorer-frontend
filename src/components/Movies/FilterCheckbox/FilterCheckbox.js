404e8afe08488e959057c39e782f5d0cf30a87ad
bevuxyna <wannhorn@gmail.com>
2022-10-02 19:31:49 +0300

    fix checkbox

--- src/components/Movies/FilterCheckbox/FilterCheckbox.js
+++ src/components/Movies/FilterCheckbox/FilterCheckbox.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function FilterCheckbox () {
+function FilterCheckbox ({onSubmitCheckbox}) {
+    const [isChecked, setIsChecked] = useState(false);
+
+    function handleChangeCheckbox() {
+        setIsChecked(!isChecked);
+        onSubmitCheckbox(!isChecked);
+    }
+
     return (
             <label className="filter-checkbox">
                 <div className="filter-checkbox__container">
-                    <input className="filter-checkbox__input" type="checkbox"/>
+                    <input
+                        className="filter-checkbox__input"
+                        type="checkbox"
+                        checked={isChecked}
+                        onChange={handleChangeCheckbox}
+                    />
                     <span className="filter-checkbox__tumbler" />
                 </div>
                 <p className="filter-checkbox__text">Короткометражки</p>
--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import searchIcon from '../../../images/search_icon.svg';
 
-function SearchForm () {
+function SearchForm ({onSearch, onSubmitCheckbox}) {
+    const [inputValue, setInputValue] = useState("");
+
+    function handleInputChange(evt) {
+        setInputValue(evt.target.value);
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        onSearch(inputValue);
+    }
+
     return (
         <div className="search-form">
-            <form className="search-form__container">
+            <form
+                className="search-form__container"
+                onSubmit={handleSubmit}
+            >
                 <img className="search-form__container_icon" src={searchIcon} alt="Лупа"></img>
-                <input className="search-form__container_input" placeholder="Фильм" required></input>
-                <button className="search-form__container_button" type="submit" />
+                <input
+                    className="search-form__container_input"
+                    placeholder="Фильм"
+                    required
+                    name="movie"
+                    type="text"
+                    value={inputValue || ""}
+                    onChange={handleInputChange}
+                ></input>
+                <button
+                    className="search-form__container_button"
+                    type="submit"
+                />
             </form>
 
-            <FilterCheckbox/>
+            <FilterCheckbox
+                onSubmitCheckbox={onSubmitCheckbox}
+            />
         </div>
     )
 }
