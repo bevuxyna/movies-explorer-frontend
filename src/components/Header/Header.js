58ddbb6a3629621e912a2dbe96b3e87c071b325f
bevuxyna <wannhorn@gmail.com>
2022-10-02 16:15:07 +0300

    fix Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,18 +4,28 @@ import {Link, NavLink, Route, Switch} from 'react-router-dom';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
 import Navigation from '../Navigation/Navigation';
 
-function Header() {
+function Header({loggedIn}) {
     return (
         <header className="header">
             <Switch>
                 <Route exact path="/">
-                    <div className="header__container">
-                        <Link to='/'><img src={logo} alt="Логотип" className="logo" /></Link>
-                        <div className="header__navigation">
-                            <NavLink to="/sign-up" className="header__register-link">Регистрация</NavLink>
-                            <NavLink to="/sign-in" className="header__login-link">Войти</NavLink>
+                    {loggedIn ? (
+                        <div className="header__auth-container">
+                            <Link to='/'><img src={logo} alt="Логотип" className="logo" /></Link>
+                            <div className="header__auth-navigation">
+                                <Navigation/>
+                            </div>
+                            <button className="header__burger-menu-button" type="button" />
                         </div>
-                    </div>
+                    ) : (
+                        <div className="header__container">
+                            <Link to='/'><img src={logo} alt="Логотип" className="logo" /></Link>
+                            <div className="header__navigation">
+                                <NavLink to="/sign-up" className="header__register-link">Регистрация</NavLink>
+                                <NavLink to="/sign-in" className="header__login-link">Войти</NavLink>
+                            </div>
+                        </div>
+                    )}
                 </Route>
 
                 <Route exact path={["/movies", "/saved-movies", "/profile"]}>
