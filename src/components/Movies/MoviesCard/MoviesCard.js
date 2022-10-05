4135c00aae6b79526e0d2737b5c6b1928959c585
bevuxyna <wannhorn@gmail.com>
2022-10-05 21:21:49 +0300

    commit

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -18,40 +18,40 @@ function MoviesCard({movie, onSaveMovie, onDeleteMovie, savedMovies}) {
     }
 
     return (
-            <li className="movies-card">
-                <a href={movie.trailerLink} target="blank">
-                    <img
-                        className="movies-card__image"
-                        src={movie.image.url ? `${BASE_BEATFILMMOVIES_URL}/${movie.image.url}` : movie.image}
-                        alt={`Постер фильма "${movie.nameRU}"`}
+        <li className="movies-card">
+            <a href={movie.trailerLink} target="blank">
+                <img
+                    className="movies-card__image"
+                    src={movie.image.url ? `${BASE_BEATFILMMOVIES_URL}/${movie.image.url}` : movie.image}
+                    alt={`Постер фильма "${movie.nameRU}"`}
+                />
+            </a>
+
+            <div className="movies-card__info">
+                <h2 className="movies-card__container-title">{movie.nameRU}</h2>
+                <p className="movies-card__container-duration">{handleMovieDuration(movie.duration, movie)}</p>
+            </div>
+
+            <div className="movies-card__button">
+                <Route path="/movies">
+                    <button
+                        className={isSaved ? "movies-card__button-saved" : "movies-card__button-save"}
+                        type="button"
+                        onClick={handleSaveMovie}
+                    >
+                        {!isSaved ? "Сохранить" : ""}
+                    </button>
+                </Route>
+
+                <Route path="/saved-movies">
+                    <button
+                        className="movies-card__button-delete"
+                        type="button"
+                        onClick={handleDeleteMovie}
                     />
-                </a>
-
-                <div className="movies-card__info">
-                    <h2 className="movies-card__container-title">{movie.nameRU}</h2>
-                    <p className="movies-card__container-duration">{handleMovieDuration(movie.duration, movie)}</p>
-                </div>
-
-                <div className="movies-card__button">
-                    <Route path="/movies">
-                        <button
-                            className={isSaved ? "movies-card__button-saved" : "movies-card__button-save"}
-                            type="button"
-                            onClick={handleSaveMovie}
-                        >
-                            {!isSaved ? "Сохранить" : ""}
-                        </button>
-                    </Route>
-
-                    <Route path="/saved-movies">
-                        <button
-                            className="movies-card__button-delete"
-                            type="button"
-                            onClick={handleDeleteMovie}
-                        />
-                    </Route>
-                </div>
-            </li>
+                </Route>
+            </div>
+        </li>
     );
 }
 
